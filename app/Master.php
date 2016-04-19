ab2daf091faeb4a9921f87df94bc0df9227315ed
jundialwan <jundi.alwan@gmail.com>
2016-04-19 12:31:01 +0700

    commenting Master.php

--- app/Master.php
+++ app/Master.php
@@ -5,6 +5,13 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use DB;
 
+/**
+ * Kelas Master
+ *
+ * Model Master merupakan model yang memiliki fungsi sebagai
+ * Master model yang memiliki method general yang dapat langsung diakses
+ * pada semua controller
+ */
 class Master extends Model
 {
 	protected $table = '';
@@ -18,6 +25,7 @@ class Master extends Model
 	 */
     public static function getLast($table, $orderby, $where = [])
     {    	
+        // check if there is addition of filter gien by user
     	if (count($where) > 0) {
     		return DB::table($table)->where($where)->orderby($orderby, 'desc')->first();            
         } else {
@@ -26,7 +34,7 @@ class Master extends Model
     }
 
     /**
-	 * Get last element on table and filter specified
+	 * Get last ID on table and filter specified
 	 * @param  String $table   table name
 	 * @param  String $orderby column name
 	 * @param  array  $where   where clause array
@@ -35,14 +43,19 @@ class Master extends Model
     public static function getLastId($table, $orderby, $where = [])
     {   
     	$lastObj;
+
+        // check if there is addition of filter gien by user
     	if (count($where) > 0)
     		$lastObj = DB::table($table)->where($where)->orderby($orderby, 'desc')->first();            
         else
     		$lastObj = DB::table($table)->orderby($orderby, 'desc')->first();            
         
+        // chekc if lastObj is null
         if ($lastObj == null)
+            // lastObj null, there are no object exist specified
         	return 0;
         else
+            // get lastObj Id
         	return $lastObj->$orderby;
     }
 }
