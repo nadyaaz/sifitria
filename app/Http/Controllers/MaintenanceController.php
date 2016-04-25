2e2dd03b23a80c65ed3c0fda2dbbead201f7ddae
jundialwan <jundi.alwan@gmail.com>
2016-04-25 14:50:42 +0700

    add function to MaintenanceController & PengadaanController

--- app/Http/Controllers/MaintenanceController.php
+++ app/Http/Controllers/MaintenanceController.php
@@ -7,5 +7,42 @@ use App\Http\Requests;
 
 class MaintenanceController extends MasterController
 {
-    //
+	/**
+	 * [getMaintenance description]
+	 * @return [type] [description]
+	 */
+    public function getMaintenance()
+    {
+    	# code...
+    }
+
+    /**
+     * [createMaintenance description]
+     * @param  Request $request [description]
+     * @return [type]           [description]
+     */
+    public function createMaintenance(Request $request)
+    {
+    	# code...
+    }
+
+    /**
+     * [updateMaintenance description]
+     * @param  Request $request [description]
+     * @return [type]           [description]
+     */
+    public function updateMaintenance(Request $request)
+    {
+    	# code...
+    }
+
+    /**
+     * [removeMaintenance description]
+     * @param  Request $request [description]
+     * @return [type]           [description]
+     */
+    public function removeMaintenance(Request $request)
+    {
+    	# code...
+    }
 }
--- app/Http/Controllers/PengadaanController.php
+++ app/Http/Controllers/PengadaanController.php
@@ -7,5 +7,42 @@ use App\Http\Requests;
 
 class PengadaanController extends MasterController
 {
-    //
+	/**
+	 * [getPengadaan description]
+	 * @return [type] [description]
+	 */
+    public function getPengadaan()
+    {
+    	# code...
+    }
+
+    /**
+     * [createPengadaan description]
+     * @param  Request $request [description]
+     * @return [type]           [description]
+     */
+    public function createPengadaan(Request $request)
+    {
+    	# code...
+    }
+
+    /**
+     * [updatePengadaan description]
+     * @param  Request $request [description]
+     * @return [type]           [description]
+     */
+    public function updatePengadaan(Request $request)
+    {
+    	# code...
+    }
+
+    /**
+     * [removePengadaan description]
+     * @param  Request $request [description]
+     * @return [type]           [description]
+     */
+    public function removePengadaan(Request $request)
+    {
+    	# code...
+    }
 }
